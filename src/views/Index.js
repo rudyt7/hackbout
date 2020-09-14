afcc257357142fbf2abb6b1a3fcea54854b5f142
rudyt7 <gallacticsoccer@gmail.com>
2020-09-15 01:34:59 +0530

    removed sections

--- src/views/Index.js
+++ src/views/Index.js
@@ -1,45 +1,41 @@
-import React from "react";
-import AOS from "aos";
-
-// reactstrap components
-// import {
-// } from "reactstrap";
+import React from 'react';
+import AOS from 'aos';
 
 // core components
-import IndexNavbar from "components/Navbars/IndexNavbar.js";
-import IndexHeader from "components/Headers/IndexHeader.js";
-import DarkFooter from "components/Footers/DarkFooter.js";
+import IndexNavbar from 'components/Navbars/IndexNavbar.js';
+import IndexHeader from 'components/Headers/IndexHeader.js';
+import DarkFooter from 'components/Footers/DarkFooter.js';
 // import Banner from "components/banner/banner";
 
 // sections for this page
-import About from "./index-sections/About/About";
-import Tracks from "./index-sections/Tracks/Tracks";
-import Schedule from "./index-sections/Schedule/Schedule";
-import Mentors from "./index-sections/Mentors/Mentors";
-import Sponsors from "./index-sections/Sponsors/Sponsors";
-import Faq from "./index-sections/Faq/Faq";
-import ContactUs from "./index-sections/ContactUs/ContactUs";
-import Bounties from "./index-sections/Bounties/Bounties";
-import Workshop from "./index-sections/Workshop/Workshop";
-import Prizes from "./index-sections/Prizes/Prizes";
-import HackClub from "./index-sections/HackClub/HackClub";
-import Team from "./index-sections/Team/Team";
+import About from './index-sections/About/About';
+import Tracks from './index-sections/Tracks/Tracks';
+import Schedule from './index-sections/Schedule/Schedule';
+import Mentors from './index-sections/Mentors/Mentors';
+import Sponsors from './index-sections/Sponsors/Sponsors';
+import Faq from './index-sections/Faq/Faq';
+import ContactUs from './index-sections/ContactUs/ContactUs';
+// import Bounties from "./index-sections/Bounties/Bounties";
+// import Workshop from "./index-sections/Workshop/Workshop";
+// import Prizes from "./index-sections/Prizes/Prizes";
+import HackClub from './index-sections/HackClub/HackClub';
+// import Team from "./index-sections/Team/Team";
 
 function Index() {
   React.useEffect(() => {
-    document.body.classList.add("index-page");
-    document.body.classList.add("sidebar-collapse");
-    document.documentElement.classList.remove("nav-open");
+    document.body.classList.add('index-page');
+    document.body.classList.add('sidebar-collapse');
+    document.documentElement.classList.remove('nav-open');
     window.scrollTo(0, 0);
     document.body.scrollTop = 0;
     return function cleanup() {
-      document.body.classList.remove("index-page");
-      document.body.classList.remove("sidebar-collapse");
+      document.body.classList.remove('index-page');
+      document.body.classList.remove('sidebar-collapse');
     };
   });
 
   AOS.init({
-    duration: 800
+    duration: 800,
   });
 
   return (
@@ -50,14 +46,10 @@ function Index() {
         <div className="main">
           <About />
           <Tracks />
-          <Bounties />
-          <Workshop />
-          <Prizes />
           <Schedule />
           <Mentors />
           <Sponsors />
           <HackClub />
-          <Team />
           <Faq />
           <ContactUs />
         </div>
